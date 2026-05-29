f2bec0f51185f928b7bc7b311dc12deddcd260c0
kclboop <kcllyons@gmail.com>
2026-05-29 10:38:11 -0400

    Update capture.js

--- capture.js
+++ capture.js
@@ -1,6 +1,7 @@
 const { chromium } = require('playwright');
 
 (async () => {
+
   const browser = await chromium.launch({
     headless: true
   });
@@ -12,16 +13,24 @@ const { chromium } = require('playwright');
     }
   });
 
-  // LOGIN
+  // OPEN LOGIN PAGE
   await page.goto('https://www.notion.so/login');
 
-  await page.fill('input[type="klyons1@fas.harvard.edu"]', process.env.NOTION_EMAIL);
+  // EMAIL
+  await page.fill(
+    'input[type="email"]',
+    process.env.NOTION_EMAIL
+  );
 
   await page.click('button[type="submit"]');
 
   await page.waitForTimeout(3000);
 
-  await page.fill('input[type="Z@ynK@yl@93"]', process.env.NOTION_PASSWORD);
+  // PASSWORD
+  await page.fill(
+    'input[type="password"]',
+    process.env.NOTION_PASSWORD
+  );
 
   await page.click('button[type="submit"]');
 
@@ -29,11 +38,14 @@ const { chromium } = require('playwright');
   await page.waitForTimeout(10000);
 
   // OPEN CALENDAR
-  await page.goto(process.env.https://calendar.notion.so/event/M3RjZG50c212OXByYjI0ZmVhMWtrbDczNXUvZTdhODI5MTM4MTYzZDlhMjM2OTZiMzA0YWM1N2VhMThkOGRiZTVjZTkyZjllNWE3ZTBlY2QxNjNiYThhMTM4ZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29tLzZhYjczYTI3LThkMjYtNDVhYi1hYmVjLTk3MjE0OTI2MmIzNQ==, {
-    waitUntil: 'networkidle'
-  });
+  await page.goto(
+    process.env.NOTION_CALENDAR_URL,
+    {
+      waitUntil: 'networkidle'
+    }
+  );
 
-  // WAIT FOR FULL RENDER
+  // WAIT FOR FULL LOAD
   await page.waitForTimeout(10000);
 
   // SCREENSHOT
@@ -45,4 +57,5 @@ const { chromium } = require('playwright');
   await browser.close();
 
   console.log('Calendar captured.');
+
 })();
