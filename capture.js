699aafa6677a31730cd705c635d960c295ead95b
kclboop <kcllyons@gmail.com>
2026-05-29 10:28:53 -0400

    Update capture.js

--- capture.js
+++ capture.js
@@ -15,13 +15,13 @@ const { chromium } = require('playwright');
   // LOGIN
   await page.goto('https://www.notion.so/login');
 
-  await page.fill('input[type="email"]', process.env.NOTION_EMAIL);
+  await page.fill('input[type="klyons1@fas.harvard.edu"]', process.env.NOTION_EMAIL);
 
   await page.click('button[type="submit"]');
 
   await page.waitForTimeout(3000);
 
-  await page.fill('input[type="password"]', process.env.NOTION_PASSWORD);
+  await page.fill('input[type="Z@ynK@yl@93"]', process.env.NOTION_PASSWORD);
 
   await page.click('button[type="submit"]');
 
@@ -29,7 +29,7 @@ const { chromium } = require('playwright');
   await page.waitForTimeout(10000);
 
   // OPEN CALENDAR
-  await page.goto(process.env.NOTION_CALENDAR_URL, {
+  await page.goto(process.env.https://calendar.notion.so/event/M3RjZG50c212OXByYjI0ZmVhMWtrbDczNXUvZTdhODI5MTM4MTYzZDlhMjM2OTZiMzA0YWM1N2VhMThkOGRiZTVjZTkyZjllNWE3ZTBlY2QxNjNiYThhMTM4ZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29tLzZhYjczYTI3LThkMjYtNDVhYi1hYmVjLTk3MjE0OTI2MmIzNQ==, {
     waitUntil: 'networkidle'
   });
 
